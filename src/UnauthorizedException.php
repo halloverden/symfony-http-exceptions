0b28c01333658075f76d36815d208d9e471e0db7
Tom Roar Furunes <tom.roar@halloverden.no>
2020-01-23 13:02:35 +0100

    ADD: exceptions

---
+++ src/UnauthorizedException.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace HalloVerden\HttpExceptions;
+
+use HalloVerden\HttpExceptions\Http\UnauthorizedHttpException;
+
+/**
+ * Class UnauthorizedException
+ *
+ * @package HalloVerden\HttpExceptions
+ */
+class UnauthorizedException extends UnauthorizedHttpException {
+
+  /**
+   * UnauthorizedException constructor.
+   *
+   * @param string          $message
+   * @param \Exception|null $previous
+   * @param int             $code
+   */
+  public function __construct(string $message = 'UNAUTHORIZED', \Exception $previous = null, $code = 0) {
+    parent::__construct($message, null, $previous, $code);
+
+    $this->setData([
+      'error' => $message
+    ]);
+  }
+
+}
